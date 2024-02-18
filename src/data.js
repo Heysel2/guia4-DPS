f7cc8a7d538ac00817fe3cfccd6040fe910c7ef7
heysel2 <heyselhrnndz@gmail.com>
2024-02-17 21:40:24 -0600

    guia4

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const data=[
+    {
+    id:1,
+    title:'Cien años de soledad',
+    price:100,
+    urlImage:'https://images.penguinrandomhouse.com/cover/9780525562443',
+    quantity:1,
+    info:'Narra la saga de la familia Buendía en Macondo, una aldea mítica donde se entrelazan magia y realidad. Gabriel García Márquez teje una historia épica sobre amor, tragedia y destino, explorando la condición humana a través de generaciones marcadas por la soledad y la búsqueda de identidad.',},
+
+    {
+    id:2,
+    title:'El señor de los anillos(Trilogía)',
+    price:190,
+    urlImage:'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg', 
+    quantity:1,
+    info:'Es una trilogía épica de J.R.R. Tolkien que sigue la odisea de un humilde hobbit llamado Frodo para destruir un anillo maligno y salvar a la Tierra Media de la oscuridad. Llena de aventuras, batallas épicas y personajes inolvidables, es un viaje inolvidable hacia la redención y la valentía.',},
+
+    {
+    id:3,
+    title:'Cuentos de Barro',
+    price:30,
+    urlImage:'https://www.librosdelaballena.com/wp-content/uploads/2020/05/cuentos-barro-244x300.png',
+    quantity:1,
+    info:'Es una colección de relatos cortos escrita por Salvador Salazar Arrué, conocido como Salarrué. Publicada en 1933, esta obra maestra de la literatura salvadoreña captura la vida rural y las tradiciones de El Salvador, tejiendo mitos, leyendas y realismo mágico en una narrativa evocadora y poética.',},
+
+    {
+    id:4,
+    title:'Tierra de Infancia',
+    price:30,
+    urlImage:'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+    quantity:1,
+    info:'Es una novela que explora la infancia y la amistad en un pequeño pueblo costero. A través de los ojos de los niños, se revelan secretos, aventuras y la magia de la inocencia. Con una prosa conmovedora, la autora captura la esencia de la niñez y la nostalgia.',},
+
+    {
+    id:5,
+    title:'Harry Potter Pack',
+    price:390,
+    urlImage:'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+    quantity:1,
+    info:'Es un conjunto de productos inspirados en la famosa serie de libros y películas de Harry Potter. Incluye elementos como libros, películas, juegos de mesa, ropa, accesorios y objetos de colección relacionados con el universo mágico creado por J.K. Rowling.',},
+
+    {
+        id:6,
+        title:'El Principito',
+        price:450,
+        urlImage:'https://cdn.culturagenial.com/es/imagenes/el-principito-portada-cke.jpg',
+        quantity:1,
+        info:'Es una novela corta escrita por Antoine de Saint-Exupéry. Narra el encuentro de un piloto estrellado en el desierto del Sahara con un joven príncipe proveniente de un pequeño asteroide. A través de su viaje, explora temas como la amistad, la soledad y el sentido de la vida. ',},
+
+        {
+        id:7,
+        title:'El Hombre que Paseaba los Libros',
+        price:100,
+        urlImage:'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1649169255-41GpR9peXjL._SL500_.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        quantity:1,
+        info:'Es un relato misterioso sobre un anciano solitario que deambula por las calles con una mochila llena de libros. Nadie sabe de dónde viene ni hacia dónde va, pero su presencia inspira curiosidad y reflexión en quienes lo encuentran, dejando un aura enigmática.',},
+
+        {
+        id:8,
+        title:'La Sombra del Viento',
+        price:230,
+        urlImage:'https://cdn.culturagenial.com/es/imagenes/la-sombra-del-viento-portada-cke.jpg',
+        quantity:1,
+        info:'De Carlos Ruiz Zafón es una novela ambientada en la Barcelona de posguerra, donde el joven Daniel Sempere descubre un misterioso libro en el Cementerio de los Libros Olvidados. Con intriga, romance y secretos, explora el poder de los libros para transformar vidas y destinos.',},
+        
+        {
+        id:9,
+        title:'Un Hombre Llamado Cervantes',
+        price:250,
+        urlImage:'https://grupoalmuzara.com/libro/9788415870944_portada.jpg',
+        quantity:1,
+        info:'Es una obra que explora la vida y obra de Miguel de Cervantes, autor de "Don Quijote". Revela sus luchas, sueños y el legado perdurable de su genio literario que trasciende el tiempo, cautivando a lectores de todas las épocas con su imaginación inigualable.',},
+
+        {
+            id:9,
+            title:'La conquista de la ironia',
+            price:340,
+            urlImage:'https://www.unebook.es/blog/wp-content/uploads/2021/02/Miguel_de_Cervantes_El_triunfo_de_la_ironia.jpg',
+            quantity:1,
+            info:'Es una obra literaria que explora el viaje de la mente humana hacia la comprensión y el uso de la ironía como herramienta de entendimiento y crítica social. A través de relatos y reflexiones, el autor navega por las complejidades del humor y la percepción.',},    
+        
+];
